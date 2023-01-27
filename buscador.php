af9f8c33f54a861a1c054840109b33006a5c0e03
sfbdata <sfb.geral@gmail.com>
2023-01-27 09:17:20 -0300

    first commit

---
+++ buscador.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client();
+
+$response = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao');
+
+$html = $response->getBody();
+
+$crawler = new Crawler;
+
+$crawler->addHtmlContent($html);
+
+$cursos = $crawler->filter('span.card-curso__nome');
+
+
+foreach ($cursos as $curso) {
+    echo $curso->textContent . PHP_EOL;
+}
